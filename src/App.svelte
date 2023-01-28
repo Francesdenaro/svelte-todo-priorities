8fed182726d8258c709806c244c693990c85e3b2
Francesco <fdenaro96@gmail.com>
2023-01-29 00:54:24 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import AddTodo from './components/AddTodo.svelte'
+	import TodoList from './components/TodoList.svelte'
+</script>
+
+<main
+	class="bg-blue-500 py-8 flex flex-col items-center gap-10 w-full h-screen"
+>
+	<TodoList />
+	<AddTodo />
+</main>
